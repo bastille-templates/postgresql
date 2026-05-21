c3786f374e83e87690a9f5e468da4777723ab317
Asep Ready <savalunova@gmail.com>
2026-05-21 17:14:47 +0700

    Add initdb.sh script for PostgreSQL data directory initialization and update Bastillefile

---
+++ root/initdb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PG_DATA="/var/db/postgres/data${PG_VERSION}"
+
+if [ -d "$PG_DATA" ]; then
+    echo "Existing data directory found → skipping initialization"
+else
+    SERVICE postgresql initdb
+fi
