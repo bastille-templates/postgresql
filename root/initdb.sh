fdd141c0d446c048c41091f8ac6bd7e650a1a782
Asep Ready <savalunova@gmail.com>
2026-05-22 23:15:05 +0700

    Remove initdb.sh script and simplify PostgreSQL initialization in Bastillefile

--- root/initdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PG_DATA="/var/db/postgres/data${PG_VERSION}"
-
-if [ -d "$PG_DATA" ]; then
-    echo "Existing data directory found → skipping initialization"
-else
-    service postgresql initdb
-fi
